495923a425d782cf92e22f07d8d2909a01a7b328
Sebastian SOLDANO <sebas.soldano@gmail.com>
2020-05-26 22:12:42 -0300

    Octava Version - +soft Scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-alert("Hola! Bienvenido")
+$(function(){
 
-$(function () {
+  $("a").click(function(event){
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var gato = this.hash;
+
+    $("html, body").animate({
+      scrollTop: $(gato).offset().top
+    }, 800, function(){
+      window.location.hash = gato;
+    });
+  }
+});
+
+
+
   $('[data-toggle="popover"]').popover()
-})
+
+});
